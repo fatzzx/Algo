adf2e0b305710c8680fd1bdf40ca6a7d2fbe0117
fatzzx <felipespinolafarias1@gmail.com>
2023-09-03 11:13:12 -0300

    E

---
+++ sbc2023/e.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int soma(int num){
+  int somaAlgs = 0;
+  while(num > 0){
+    somaAlgs += num % 10;
+    num /= 10;
+  }
+  return somaAlgs;
+}
+
+int main(){
+  int n, k, e, r, t;
+  priority_queue<int> maxHeap;
+  cin >> n >> k;
+  for(int i = 0; i < n; i++){
+    cin >> e;
+    maxHeap.push(e);
+  }  
+
+  for(int i = 0; i < k; i++){
+    t = maxHeap.top();
+    r = soma(t);
+    maxHeap.pop();
+    maxHeap.push(t - r);
+  }
+
+  cout << r << endl;
+
+  return 0;
+}
